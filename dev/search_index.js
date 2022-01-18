dad033a1554838796b6d13e5210f4705527be7d4
Documenter.jl <documenter@juliadocs.github.io>
2022-01-18 16:36:00 +0000

    build based on 1cb9380

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NumPy","category":"page"},{"location":"#NumPy","page":"Home","title":"NumPy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NumPy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NumPy]","category":"page"}]
+}
